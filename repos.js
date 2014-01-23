30ada939e21f117b601953a712e30a801f8b3d6c
sdeering <samdeering@gmail.com>
2014-01-23 22:34:14 +1000

    updates to get repos list

--- repos.js
+++ repos.js
@@ -1,92 +1,215 @@
 'use strict';
 
 /*
-	Cleverstack Module Repo Finder.
+    Cleverstack Module Repo Finder.
+    http://cleverstack.io/modules
 
-	1. Search for official & community modules
-	2. npm, github & bower
-	3. Keywords:
+    1. Gets official modules list for cleverstack
+    2. Searches github
+    3. Filters by keywords
 
-	cleverstack-module
-	cleverstack-seed
-	/+ frontend
-	/+ backend
+    todo:
+    - npm publish check
+    - community module scout bot
+    - persistent caching
 
 */
 
 var Q = require( 'q' );
 var request = require( 'request' );
 var _ = require( 'lodash' );
-var keepProperties = [ 'name','description','html_url','git_url','created_at','updated_at','forks','stargazers_count' ];
-var keepKeywords = [ 'cleverstack-module','cleverstack-seed' ];
-var repos = [];
+var async = require( 'async' );
+var throat = require( 'throat' );
+var cachedResults;
+
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || 'ba861503f44aa44be939';
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'fb7d5a721bea1e5c9292765835b342c3e68b3ef6';
+var csOrgs = [ 'cleverstack' ];
+var csKeywords = [ 'cleverstack-module', 'cleverstack-seed' ]; //(additions: frontend, backend)
 
 
 // API / GET
 exports.list = function( req, res ) {
 
     //caching
-    if (!_.isEmpty(repos)) {
-        res.json( repos );
+    if ( !_.isEmpty( cachedResults ) ) {
+        res.json( cachedResults );
         return true;
     }
-    exports.newList( req, res );
+    exports.refresh( req, res );
 
 };
 
-exports.newList = function( req, res ) {
+exports.refresh = function( req, res ) {
 
-	GIT.getOfficialRepos( function( results ) {
-		res.json( results );
-	});
+    res.send( 'Regenerating results cache...' );
+    _getRepos();
 
 };
 
 // Search GitHub
-var GIT = {
+function  _getRepos() {
 
-    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID || 'ba861503f44aa44be939',
-	GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET || 'fb7d5a721bea1e5c9292765835b342c3e68b3ef6',
-	orgs: [ 'cleverstack', 'clevertech' ],
+    var apiLimitExceeded = false;
 
-	getOfficialRepos: function( done ) {
+    return Q.fcall(function () {
 
-		this.orgs.forEach( function( org ) {
-			repos.push( _fetchGitOrgRepos( org ) );
-		});
+        var deferred = Q.defer();
+        var options = {
+            url: 'https://api.github.com/orgs/'+csOrgs+'/repos',
+            qs: {
+                client_id: GITHUB_CLIENT_ID,
+                client_secret: GITHUB_CLIENT_SECRET
+            },
+            headers: {
+                'User-Agent': 'cleverstack list'
+            },
+            json: true,
+            timeout: 60000
+        };
+        console.log( options.url );
+        request.get( options, function ( err, res, body ) {
 
-		Q.all( repos ).then( function( ful ) {
-			ful = _.flatten( ful );
-			done( ful );
-		});
+            if ( !err && body && /API Rate Limit Exceeded/.test(body.message) ) {
 
-	}
-};
+                apiLimitExceeded = true;
+                deferred.resolve();
 
-// get the repo list for the organisation
-function _fetchGitOrgRepos( org ) {
-	var deferred = Q.defer();
+            }
+            deferred.resolve( body );
+        });
 
-	var options = {
-	    url: 'https://api.github.com/orgs/'+org+'/repos',
-	    headers: {
-	        'User-Agent': 'request'
-	    },
-	    json: true
-	};
-	console.log( options.url );
+        return deferred.promise;
 
-	request(options, function ( err, res, body ) {
+    }).then( function( repos ) {
 
-		if ( !!err ) {
-			deferred.reject( error );
-		}
+        var results = repos.map(throat(10, function ( repo ) {
 
-		if (!err && res.statusCode == 200) {
-			deferred.resolve( body );
-		}
+            var deferred = Q.defer();
+            var userName = repo.owner.login;
+            var repoName = repo.name;
+            var options = {
+                url: 'https://api.github.com/repos/'+userName+'/'+repoName,
+                qs: {
+                    client_id: GITHUB_CLIENT_ID,
+                    client_secret: GITHUB_CLIENT_SECRET
+                },
+                headers: {
+                    'User-Agent': 'cleverstack list'
+                },
+                json: true,
+                timeout: 60000
+            };
+            console.log( 'fetching '+options.url );
 
-	});
+            request.get( options, function ( err, res, body ) {
 
-	return deferred.promise;
+                if ( !err && body && /API Rate Limit Exceeded/.test(body.message) ) {
+
+                    apiLimitExceeded = true;
+                    deferred.resolve();
+
+                } else if ( body && /Repository access blocked/.test(body.message) ) {
+
+                    deferred.resolve();
+
+                } else if ( !err && res.statusCode === 200 ) {
+
+                    var complete = function ( keywords ) {
+
+                        var model = _createModuleData( body, keywords );
+                        csKeywords.forEach( function( k ) {
+
+                            //http://jsperf.com/jquery-inarray-vs-underscore-indexof/18
+                            for (var i = 0, m = keywords.length; i < m; i++) {
+                                if (keywords[i] === k) {
+                                    cachedResults.push( model );
+                                    console.log( 'adding '+model.name );
+                                    // deferred.resolve( model );
+                                    return true;
+                                }
+                            }
+
+                        });
+                        deferred.resolve( false );
+
+                    };
+
+                    _fetchRepoKeywords(body.owner.login, body.name, 'package.json', function ( err, keywords ) {
+
+                        if ( err == false ) {
+                            complete( keywords );
+                        }
+
+                    });
+                    return;
+
+                } else {
+
+                    if ( response && response.statusCode === 404 ) {
+                        deferred.resolve();
+                    } else {
+                        console.log('err github fetch', org, response.statusCode, err, body);
+                        deferred.resolve();
+                    }
+
+                }
+
+                return deferred.promise;
+            });
+
+            return deferred.promise;
+        }));
+
+    }).then( function ( results ) {
+
+        if ( apiLimitExceeded ) {
+
+            console.log( 'API limit exceeded. Using cached GitHub results.' );
+            return Q.all( cachedResults );
+
+        }
+
+        cachedResults = _.unique( _.compact( results ) );
+        return Q.all( results );
+
+    });
+
+}
+
+// construct the module data we need
+function _createModuleData( data, keywords ) {
+
+    var obj = {
+        name: data.name,
+        description: data.description,
+        owner: data.owner.login,
+        website: data.html_url,
+        forks: data.forks,
+        stars: data.watchers,
+        created: data.created_at,
+        updated: data.pushed_at
+    };
+
+    if ( keywords ) {
+        obj.keywords = keywords;
+    }
+
+    return obj;
+}
+
+// get keywords from package.json
+function _fetchRepoKeywords( user, repo, file, cb ) {
+
+    var url = 'https://raw.github.com/' + user + '/' + repo + '/master/'+ file;
+    console.log( 'fetching '+url );
+    request.get(url, {json: true}, function ( err, res, body ) {
+        if ( !err && body && body.keywords ) {
+            // console.log(body.keywords);
+            cb( false, body.keywords );
+        } else {
+            cb( true );
+        }
+    });
+
 }
--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 'use strict';
 
-var http = require('http');
-var express = require('express');
-var Q = require('q');
-var crypto = require('crypto');
+var http = require( 'http' );
+var express = require( 'express' );
+var Q = require( 'q' );
+var crypto = require( 'crypto' );
 var app = express();
-var repos = require('./repos');
+var repos = require( './repos' );
 var HTTP_PORT = process.env.PORT || 8011;
+var UPDATE_CACHE_INTERVAL_IN_HOURS = 1;
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
+//Express
+app.configure( function () {
+    app.use( express.logger( 'dev' ) );     /* 'default', 'short', 'tiny', 'dev' */
+	app.use( express.json() );
+	app.use( express.compress() );
+	app.use( express.urlencoded() );
+
+    // Enable CORS
+    app.use(function( req, res, next ) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "x-requested-with, content-type");
+        res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Credentials", "true");
+        res.header("Access-Control-Max-Age", "1000000000");
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        }
+        else {
+            next();
+        }
+    });
 });
-app.get('/', repos.list);
-app.get('/refresh', repos.newList);
 
-app.listen(HTTP_PORT);
+//API
+app.get( '/', repos.list );
+app.get( '/refresh', repos.refresh );
 
-console.log('Server running on port ' + HTTP_PORT);
+app.listen( HTTP_PORT );
+console.log( 'Server running on port ' + HTTP_PORT );
+
+//interval for refreshing results cache
+setInterval(repos.list, UPDATE_CACHE_INTERVAL_IN_HOURS * 60 * 60 * 1000);
